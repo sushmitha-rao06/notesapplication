9b50ee7f5f845386b5c4aededd57e5c4a2405885
sushmi1997 <sushmitha.kirun@gmail.com>
2025-03-03 15:31:02 -0500

    new feactures added

--- db.py
+++ db.py
@@ -65,3 +65,31 @@ class NotesDB():
         return True
     
     
+    
+# ///////////////////reportss////////////////////////////////////
+
+# ///////////////////////////////////////////////////////////////
+
+
+  def save_notes_of_user(self,id, notes):
+        try:
+
+            SQL = ''' INSERT INTO DEVDB.dbo.susmitha_notes_table(userId, notes)VALUES( ?,? );'''
+            cursor = self.cursor.execute(SQL, (id, notes))
+            cursor.commit()
+        except Exception as e :
+            print("Exception occured on saving the database--",e)
+            return False
+        return True
+    
+    def delete_notes_of_user(self,id):
+        try:
+
+            SQL = ''' DELETE FROM DEVDB.dbo.susmitha_notes_table Where id = ( ?);'''
+            cursor = self.cursor.execute(SQL, id)
+            cursor.commit()
+        except Exception as e :
+            print("Exception occured on saving the database--",e)
+            return False
+        return True
+    
